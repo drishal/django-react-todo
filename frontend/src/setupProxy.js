3987cc203a1aea91ab6338b63b9890e7b3778dcc
drishal <drishalballaney@gmail.com>
2022-10-19 00:15:43 +0530

    init commit

---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,12 @@
+ const { createProxyMiddleware } = require('http-proxy-middleware');
+
+ module.exports = function(app) {
+   app.use(
+     '/api/todos/',
+     createProxyMiddleware({
+       target: 'http://127.0.0.1:8000',
+       // target: 'http://:8000',
+       changeOrigin: true,
+     })
+   );
+ };
